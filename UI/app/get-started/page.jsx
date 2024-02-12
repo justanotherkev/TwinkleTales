259fa781276ac0ae7d9ec2e10c75104ad4dff099
Kevin Augustin <kevinaugustin41@gmail.com>
2024-02-12 11:15:38 +0530

    Removed 'get-started' directory

--- UI/app/get-started/page.jsx
+++
@@ -1,3 +0,0 @@
-export default function GetStarted() {
-	return <h1>Get started</h1>;
-}
--- UI/app/page.jsx
+++ UI/app/page.jsx
@@ -12,24 +12,18 @@ export default function Home() {
 			<div className={s.body}>
 				<div className={s.image}></div>
 
-
 				<div className={s.get_started}>
-				
-				
 					<div className={s.get_started_text}>
 						<h2>Get started</h2>
 						<p>
 							Explore captivating stories tailored to your child's imagination
 						</p>
 					</div>
-				
+
 					<div className={s.get_started_actions}>
 						<Link href="./get-started/login">Login</Link>
 						<Link href="./get-started/sign-up">Sign Up</Link>
 					</div>
-				
-				
-				
 				</div>
 			</div>
 		</div>
