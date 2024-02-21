40b08e5269d96adf253d23c75f915ae69954984a
Kevin Augustin <kevinaugustin41@gmail.com>
2024-02-21 08:36:48 +0530

    Added components to the Get Started page

--- UI/app/page.jsx
+++ UI/app/page.jsx
@@ -1,33 +1,13 @@
-import HeaderTitle from "@/components/header-title/header-title";
-import s from "./page.module.css";
-import Link from "next/link";
+import PageComponent from "@/components/page-component/page-component";
+import GetStartedBox from "@/components/get-started-box/get-started-box";
 
 export default function Home() {
-  return (
-    <div className={s.home}>
-      <HeaderTitle />
-      <div className={s.body}>
-        <div className={s.image}></div>
-        <div className={s.get_started}>
-          <div className={s.get_started_text}>
-            <h2>Get started</h2>
-            <p>
-              Explore captivating stories tailored to your child's imagination
-            </p>
-          </div>
-
-          <div className={s.get_started_actions}>
-            <Link href="./login" className={s.link}>
-              <div className={s.link_button}>Login</div>
-            </Link>
-            <Link href="./sign-up" className={s.link}>
-              <div className={s.link_button}>Sign Up</div>
-            </Link>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<PageComponent
+			src={"/get-started-img.png"}
+			form_component={<GetStartedBox />}
+		/>
+	);
 }
 
 // import sound from "../public/welcome_background_music.mp3";
