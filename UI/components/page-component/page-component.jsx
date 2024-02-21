bd95eb5819fc0eb2d6247a450a61b675a4e46cb0
Kevin Augustin <kevinaugustin41@gmail.com>
2024-02-21 08:31:53 +0530

    Created page-component

---
+++ UI/components/page-component/page-component.jsx
@@ -0,0 +1,17 @@
+import HeaderTitle from "../header-title/header-title.jsx";
+import s from "./page-component.module.css";
+import Image from "next/image";
+
+export default function PageComponent(props) {
+	return (
+		<div className={s.home}>
+			<HeaderTitle />
+			<div className={s.body}>
+				<div className={s.image}>
+					<Image src={props.src} width={650} height={650} />
+				</div>
+				<div className={s.form_box}>{props.form_component}</div>
+			</div>
+		</div>
+	);
+}
