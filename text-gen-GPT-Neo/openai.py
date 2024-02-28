416741baf2645d42e02febb6201377ace076b2fd
Mehansa Abeywardena <orshadhi@gmail.com>
2024-02-28 11:55:36 +0530

    Changing the truncation parameter value.

--- text-gen-GPT-Neo/openai.py
+++ text-gen-GPT-Neo/openai.py
@@ -4,7 +4,8 @@ generator = pipeline('text-generation', model='EleutherAI/gpt-neo-2.7B')
 
 prompt = "Once upon a time, in a lush forest, there lived a friendly fox named Freddie and his best friend, Robbie the rabbit. They loved playing soccer together in the clearing near their cozy burrows. One sunny day, as they kicked the ball back and forth, they noticed something strange lurking in the bushes..."
 
-res = generator(prompt, truncation=True, max_length=400, do_sample=True, temperature=0.9)
+# Putting truncation from true to false to see if the story length is different 
+res = generator(prompt, truncation=False, max_length=400, do_sample=True, temperature=0.9)
 
 generated_text = res[0]['generated_text']
 
