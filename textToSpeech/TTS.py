bd128389c825cf10c9d791fd650a114b5dc3775f
AkinduUsliyanage <akinduusliyanage@gmail.com>
2024-03-03 18:09:44 +0530

    Added questions with eleven labs
    Speech generation and converting speech to text

--- textToSpeech/TTS.py
+++ textToSpeech/TTS.py
@@ -1,11 +1,45 @@
 import elevenlabs
-import ffmpeg
+import speech_recognition as stt
+
+
+#to get an idea what are the available voices in elevenlabs
 print(elevenlabs.voices())
-tts = elevenlabs.generate(
-    text = "what are the characters are in the story!"+"Where is the story taking place?"+"What is the weather like today?"+"what is your favourite sport?",
-    voice= "Domi",
 
-)
+#text to speech
+#used Domi
+#taking parameters that includes the question
+list = []
+def speak(text1):
+    tts = elevenlabs.generate(
+        text = text1,
+        voice= "Domi",
+        api_key = "ac764488fbfd187d77d484e08b31293a"
+    )
+    elevenlabs.play(tts)
 
+#converting speech to text
+def speechToText():
+    recognizer = stt.Recognizer()
+    with stt.Microphone() as source:
+        print("speak now")
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+        answer1 = recognizer.recognize_google(audio)
+        list.append(answer1)
+        print(list)
 
-elevenlabs.play(tts)
+#passing arguments into text to speech funtion and calling sppech to text funtion
+while True:
+    try:
+        speak("what are the characters are in your story?")
+        speechToText()
+        speak("Where is the story taking place?")
+        speechToText()
+        speak("What is the weather like today?")
+        speechToText()
+        speak("what is your favourite sport?")
+        speechToText()
+        break
+    except:
+        print("Sorry, I didn't understand")
+
