3ae118facf36d4050714f1bd7726dda56361eeda
Nethmi <nethmi.20220716@iit.ac.lk>
2024-03-20 23:15:30 +0530

    improved sign-up functionality

--- UI/app/sign-up/page.jsx
+++ UI/app/sign-up/page.jsx
@@ -1,11 +1,19 @@
+"use client";
 import PageComponent from "@/components/page-component/page-component";
 import SignUpBox from "@/components/sign-up-box/sign-up-box";
+import { useRouter } from "next/navigation";
 
 export default function SignUp() {
-	return (
-		<PageComponent
-			src={"/login-signup-img.png"}
-			form_component={<SignUpBox />}
-		/>
-	);
+  const router = useRouter();
+
+  const handleRouting = () => {
+    router.push("/prompt");
+  };
+
+  return (
+    <PageComponent
+      src={"/login-signup-img.png"}
+      form_component={<SignUpBox handleRouting={handleRouting} />}
+    />
+  );
 }
