85fe4f5c37d4658cbab2a6088aa33869350a5d32
Kevin Augustin <kevinaugustin41@gmail.com>
2024-03-22 14:44:27 +0530

    Completed login and signup pages with updated middleware.ts and .env.local

--- UI/components/login-box/login-box.jsx
+++ UI/components/login-box/login-box.jsx
@@ -4,85 +4,98 @@ import FormButton from "../form-button/form-button";
 import s from "./login-box.module.css";
 import { useAuth, useSignIn } from "@clerk/nextjs";
 import { useState } from "react";
+import Link from "next/link";
 
 export default function LoginBox() {
-  const { isLoaded, signIn, setActive } = useSignIn();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const router = useRouter();
+	const { isLoaded, signIn, setActive } = useSignIn();
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const router = useRouter();
 
-  // start the sign In process.
-  const handleSubmit = async (e) => {
-    console.log("Submitting credentials");
-    e.preventDefault();
+	// start the sign In process.
+	const handleSubmit = async (e) => {
+		console.log("Submitting credentials");
+		e.preventDefault();
 
-    setEmail(e.target.email.value);
-    setPassword(e.target.password.value);
+		setEmail(e.target.email.value);
+		setPassword(e.target.password.value);
 
-    if (!isLoaded) {
-      return;
-    }
+		if (!isLoaded) {
+			return;
+		}
 
-    try {
-      const result = await signIn.create({
-        identifier: email,
-        password: password,
-      });
+		try {
+			const result = await signIn.create({
+				identifier: email,
+				password: password,
+			});
 
-      if (result.status === "complete") {
-        console.log(result);
-        await setActive({ session: result.createdSessionId });
-        router.push("/prompt");
-      } else {
-        console.log(result);
-      }
-    } catch (error) {
-      console.error("error", error.errors[0].longMessage);
-    }
-  };
+			if (result.status === "complete") {
+				console.log(result);
+				await setActive({ session: result.createdSessionId });
+				router.push("/prompt");
+			} else {
+				console.log(result);
+			}
+		} catch (error) {
+			console.error("error", error.errors[0].shortMessage);
+		}
+	};
 
-  if (!isLoaded) {
-    return (
-      <div className={s.login_box}>
-        <h2 className={s.title}>Loading...</h2>
-      </div>
-    );
-  } else {
-    return (
-      <div className={s.login_box}>
-        <h2 className={s.title}>Login</h2>
+	if (!isLoaded) {
+		return (
+			<div className={s.login_box}>
+				<h2 className={s.title}>Loading...</h2>
+			</div>
+		);
+	} else {
+		return (
+			<div className={s.login_box}>
+				<h2 className={s.title}>Login</h2>
 
-        <form
-          onSubmit={handleSubmit}
-          // action="/submit"
-          // method="post"
-          className={s.form}
-        >
-          <div className={s.credentials_box}>
-            <div className={s.credential_detail}>
-              <label for="email">Email</label>
-              <input type="email" id="email" name="email" required />
-            </div>
+				<form
+					onSubmit={handleSubmit}
+					// action="/submit"
+					// method="post"
+					className={s.form}
+				>
+					<div className={s.credentials_box}>
+						<div className={s.credential_detail}>
+							<label for="email">Email</label>
+							<input
+								onChange={(e) => setEmail(e.target.value)}
+								type="email"
+								id="email"
+								name="email"
+								required
+							/>
+						</div>
 
-            <div className={s.credential_detail}>
-              <label for="password">Password</label>
-              <input type="password" id="password" name="password" required />
-            </div>
-          </div>
-
-          {/* <div class="continue-with-google">
-						<span>Continue with Google</span>
+						<div className={s.credential_detail}>
+							<label for="password">Password</label>
+							<input
+								onChange={(e) => setPassword(e.target.value)}
+								type="password"
+								id="password"
+								name="password"
+								required
+							/>
+						</div>
 					</div>
-					<div class="continue-with-facebook">
-						<span>Continue with Facebook</span>
-					</div> */}
 
-          {/* <p>Forgot password</p> */}
-          <FormButton isNav={false} name={"Login"} type={"submit"} />
-        </form>
-      </div>
-    );
-  }
+					{/* <p>Forgot password</p> */}
+
+					<FormButton isNav={false} name={"Login"} type={"submit"} />
+					<p className={s.go_to_signup}>
+						No account?{" "}
+						<span>
+							<Link href="/sign-up">Create one</Link>
+						</span>
+					</p>
+				</form>
+			</div>
+		);
+	}
 }
 
 // autofill ofr STT
--- UI/components/sign-up-box/sign-up-box.jsx
+++ UI/components/sign-up-box/sign-up-box.jsx
@@ -1,145 +1,191 @@
-("use client");
+"use client";
 import { useState } from "react";
 import FormButton from "../form-button/form-button";
 import s from "./sign-up-box.module.css";
 import { useSignUp } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export default function SignUpBox() {
-  const { isLoaded, signUp, setActive } = useSignUp();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [pendingVerification, setPendingVerification] = useState(false);
-  const [username, setUsername] = useState("");
-  const [code, setCode] = useState("");
+	const { isLoaded, signUp, setActive } = useSignUp();
 
-  const handleSubmit = async (e) => {
-    console.log("Submitting credentials...");
-    e.preventDefault();
+	const [username, setUsername] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
 
-    if (!isLoaded) {
-      return;
-    }
+	const [pendingVerification, setPendingVerification] = useState(false);
+	const [code, setCode] = useState("");
+	const [errorMsg, setErrorMsg] = useState("");
 
-    try {
-      console.log("in try block handleSubmit");
-      await signUp.create({
-        emailAddress: email,
-        password: password,
-      });
+	const router = useRouter();
 
-      await signUp.prepareEmailAddressVerification({ strategy: "email_code" });
+	const handleSubmit = async (e) => {
+		console.log("Submitting credentials...");
+		e.preventDefault();
 
-      setPendingVerification(true);
-    } catch (error) {
-      console.error(JSON.stringify(error, null, 2));
-    }
-  };
+		if (!isLoaded) {
+			return;
+		}
 
-  const verifyCode = async (e) => {
-    console.log("Verifying code...");
-    e.preventDefault();
+		try {
+			console.log("in try block handleSubmit");
+			console.log("Username: " + username);
+			console.log("Email: " + email);
+			console.log("Password: " + password);
 
-    setCode(e.target.code.value);
-    console.log("Code entered: " + code);
+			await signUp.create({
+				username: username,
+				emailAddress: email,
+				password: password,
+			});
 
-    if (!isLoaded) {
-      return;
-    }
+			// Sends the email with verification code
+			await signUp.prepareEmailAddressVerification({ strategy: "email_code" });
 
-    try {
-      console.log("in try block verifyCode");
-      const completeSignUp = await signUp.attemptEmailAddressVerification({
-        code: code,
-      });
+			setPendingVerification(true);
+			setErrorMsg("");
+		} catch (error) {
+			console.log(JSON.stringify(error, null, 2));
+			console.log(error.errors[0].message);
+			setErrorMsg(error.errors[0].message);
+		}
+	};
 
-      if (completeSignUp.status !== "complete") {
-        console.log(JSON.stringify(completeSignUp, null, 2));
-      }
+	const verifyCode = async (e) => {
+		console.log("Verifying code...");
+		e.preventDefault();
 
-      if (completeSignUp.status === "complete") {
-        await setActive({ session: completeSignUp.createdSessionId });
-        console.log("Pushing route /prompt");
-        props.handleRouting;
-      }
-    } catch (error) {
-      console.error(JSON.stringify(error, null, 2));
-    }
-  };
+		// setCode(e.target.code.value);
+		console.log("Code entered: " + code);
 
-  if (!isLoaded) {
-    return (
-      <div className={s.sign_up_box}>
-        <h2 className={s.title}>Loading...</h2>
-      </div>
-    );
-  } else {
-    return (
-      <div className={s.sign_up_box}>
-        <h2 className={s.title}>Sign Up</h2>
+		if (!isLoaded) {
+			return;
+		}
 
-        {!pendingVerification ? (
-          <form
-            onSubmit={handleSubmit}
-            // action="/submit"
-            // method="post"
-            className={s.form}
-          >
-            <div className={s.credentials_box}>
-              <div className={s.credential_detail}>
-                <label for="name">Username</label>
-                <input type="text" id="username" name="username" required />
-              </div>
+		// try {
+		console.log("in try block verifyCode");
+		const completeSignUp = await signUp.attemptEmailAddressVerification({
+			code: code,
+		});
 
-              <div className={s.credential_detail}>
-                <label for="email">Email</label>
-                <input
-                  onChange={(e) => setEmail(e.target.value)}
-                  type="email"
-                  id="email"
-                  name="email"
-                  required
-                />
-              </div>
+		if (completeSignUp.status !== "complete") {
+			console.log("Could not complete signup");
+			console.log(JSON.stringify(completeSignUp, null, 2));
+		}
 
-              <div className={s.credential_detail}>
-                <label for="password">Password</label>
-                <input
-                  onChange={(e) => setPassword(e.target.value)}
-                  type="password"
-                  id="password"
-                  name="password"
-                  required
-                />
-              </div>
-            </div>
+		if (completeSignUp.status === "complete") {
+			console.log("Signup completed");
+			await setActive({ session: completeSignUp.createdSessionId });
+			console.log("Pushing route /prompt");
+			router.push("/prompt");
+		}
+		// } catch (error) {
+		// 	console.error(JSON.stringify(error, null, 2));
+		// 	setErrorMsg(error.errors[0].message);
+		// }
+	};
 
-            <FormButton isNav={false} name={"Sign Up"} type={"submit"} />
-          </form>
-        ) : (
-          <form
-            onSubmit={verifyCode}
-            // action="/submit"
-            // method="post"
-            className={s.form}
-          >
-            <div className={s.credentials_box}>
-              <div className={s.credential_detail}>
-                <label for="code">Code</label>
-                <input
-                  onChange={(e) => setCode(e.target.value)}
-                  type="text"
-                  id="code"
-                  name="code"
-                  required
-                />
-              </div>
-            </div>
+	if (!isLoaded) {
+		return (
+			<div className={s.sign_up_box}>
+				<h2 className={s.title}>Loading...</h2>
+			</div>
+		);
+	} else {
+		return (
+			<div className={s.sign_up_box}>
+				<h2 className={s.title}>Sign Up</h2>
 
-            <FormButton isNav={false} name={"Verify Code"} type={"submit"} />
-          </form>
-        )}
-      </div>
-    );
-  }
+				{!pendingVerification ? (
+					<>
+						<form
+							onSubmit={handleSubmit}
+							action="/submit"
+							method="post"
+							className={s.form}
+						>
+							<div className={s.credentials_box}>
+								<div className={s.credential_detail}>
+									<label for="name">Username</label>
+									<input
+										onChange={(e) => setUsername(e.target.value)}
+										type="text"
+										id="username"
+										name="username"
+										required
+									/>
+								</div>
+
+								<div className={s.credential_detail}>
+									<label for="email">Email</label>
+									<input
+										onChange={(e) => setEmail(e.target.value)}
+										type="email"
+										id="email"
+										name="email"
+										required
+									/>
+								</div>
+
+								<div className={s.credential_detail}>
+									<label for="password">Password</label>
+									<input
+										onChange={(e) => setPassword(e.target.value)}
+										type="password"
+										id="password"
+										name="password"
+										required
+									/>
+								</div>
+							</div>
+
+							<FormButton isNav={false} name={"Sign Up"} type={"submit"} />
+							<p className={s.error_message}>{errorMsg}</p>
+
+							<p className={s.go_to_signup}>
+								Already have an account?{" "}
+								<span>
+									<Link href="/login">Log in</Link>
+								</span>
+							</p>
+						</form>
+					</>
+				) : (
+					<>
+						<form
+							onSubmit={verifyCode}
+							// action="/submit"
+							// method="post"
+							className={s.form}
+						>
+							<div className={s.code_welcome_message}>
+								<h3>Welcome {username}</h3>
+								<p>
+									Please enter the verification code sent to{" "}
+									<span>{email}</span>
+								</p>
+							</div>
+							<div className={s.credentials_box}>
+								<div className={s.credential_detail}>
+									<label for="code">Code</label>
+									<input
+										onChange={(e) => setCode(e.target.value)}
+										type="text"
+										id="code"
+										name="code"
+										required
+									/>
+								</div>
+							</div>
+
+							<FormButton isNav={false} name={"Verify Code"} type={"submit"} />
+							<p className={s.error_message}>{errorMsg}</p>
+						</form>
+					</>
+				)}
+			</div>
+		);
+	}
 }
+
+// <UserProdile /> component
